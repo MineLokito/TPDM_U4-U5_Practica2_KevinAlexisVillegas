6f837b57d4d256c54e7a1d95adb26968ca8e8370
MineLokito <kealvillegases@ittepic.edu.mx>
2019-12-01 08:20:23 -0700

    Modificacion a color y detalles Esteticos

--- app/src/main/java/mx/edu/ittepic/tpdm_u4_u5_practica2_kevinalexisvillegas/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/tpdm_u4_u5_practica2_kevinalexisvillegas/MainActivity.kt
@@ -1,13 +1,35 @@
 package mx.edu.ittepic.tpdm_u4_u5_practica2_kevinalexisvillegas
 
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    var locationManager : LocationManager?=null
+    var longitud:Double = 0.0
+    var latitud: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(Lienzo(this))
         getSupportActionBar()?.hide()
+
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
+        var oyenteGPS = OyenteGPS(this)
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //permiso denegado
+            Toast.makeText(this,"Active el GPS.", Toast.LENGTH_LONG).show()
+            return
+        }
+        //permiso aprobado
+        locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,oyenteGPS)
+
     }
+
 }
+
+
---
+++ app/src/main/java/mx/edu/ittepic/tpdm_u4_u5_practica2_kevinalexisvillegas/OyenteGPS.kt
@@ -0,0 +1,29 @@
+package mx.edu.ittepic.tpdm_u4_u5_practica2_kevinalexisvillegas
+
+import android.location.Location
+import android.location.LocationListener
+import android.os.Bundle
+
+
+class OyenteGPS(p:MainActivity): LocationListener {
+    var puntero = p
+    override fun onLocationChanged(location: Location) {
+        puntero.latitud = location.latitude
+        puntero.longitud = location.longitude
+        puntero.setTitle("${puntero.latitud},${puntero.longitud}")
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+
+    }
+
+
+}
